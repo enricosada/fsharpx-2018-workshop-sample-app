b7448124e4b51ed8f624c42fcf906f57c9446b69
Enrico Sada <enrico@sada.io>
2018-04-05 22:42:48 +0100

    simple app

---
+++ src/MyApp/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv =
+    let c = MyLibrary.helloMessage "F# eXchange"
+    printfn "%s" c
+
+    MyLibrary.downloadFrom "http://fsharp.org/"
+    |> Async.RunSynchronously
+    |> printfn "%s"
+
+    0 // return an integer exit code
---
+++ src/MyLib/Library1.fs
@@ -0,0 +1,11 @@
+﻿module MyLibrary
+
+open System
+
+let helloMessage name =
+    sprintf "Hello %s" name
+
+let downloadFrom url = async {
+    use wc = new System.Net.WebClient()
+    return! wc.AsyncDownloadString (Uri(url))
+}
