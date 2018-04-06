09aedd9962430ce3add0782fca6d3fab6cb8bf06
Enrico Sada <enrico@sada.io>
2018-04-06 12:58:14 +0100

    fix api

--- src/MyLib/Library1.fs
+++ src/MyLib/Library1.fs
@@ -7,8 +7,12 @@ let helloMessage name =
 
 let downloadFrom url = async {
     use wc = new System.Net.WebClient()
+#if NETSTANDARD2_0
+    return wc.DownloadString (Uri(url))
+#else
     //this is needed in .NET 4.5.2 because it use an old tls version in the example github website
     //ask Steffen Forkmann for a funny story about it 
     System.Net.ServicePointManager.SecurityProtocol <- System.Net.SecurityProtocolType.Tls12
     return! wc.AsyncDownloadString (Uri(url))
+#endif
 }
