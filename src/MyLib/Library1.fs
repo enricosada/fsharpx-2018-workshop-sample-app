82ca826c25f3017921c2ad6aefd628da940695c3
Enrico Sada <enrico@sada.io>
2018-04-06 13:48:33 +0100

    fix github url error

--- src/MyLib/Library1.fs
+++ src/MyLib/Library1.fs
@@ -7,5 +7,8 @@ let helloMessage name =
 
 let downloadFrom url = async {
     use wc = new System.Net.WebClient()
+    //this is needed in .NET 4.5.2 because it use an old tls version in the example github website
+    //ask Steffen Forkmann for a funny story about it 
+    System.Net.ServicePointManager.SecurityProtocol <- System.Net.SecurityProtocolType.Tls12
     return! wc.AsyncDownloadString (Uri(url))
 }
